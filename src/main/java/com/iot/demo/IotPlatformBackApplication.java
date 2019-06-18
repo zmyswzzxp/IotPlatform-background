ae5e8f5ef623d909515ddefeb6e71a56e698f4ca
shangweizheng <siwad@126.com>
2019-06-18 16:44:36 +0800

    入住eureka

--- src/main/java/com/iot/demo/IotPlatformBackApplication.java
+++ src/main/java/com/iot/demo/IotPlatformBackApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+@EnableDiscoveryClient //服务发现
 //@EnableCaching
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class IotPlatformBackApplication {
